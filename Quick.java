ec0a5ae562a398fbe49e38ea1787647ab92e083d
Jessica Kim <jkim00@stuy.edu>
2019-03-13 03:08:49 -0400

    added empty file, started partition, splits the numbers into greater than or equal to pivot

--- Quick.java
+++ Quick.java
@@ -2,7 +2,26 @@ public class Quick {
   /*
   *@return the index of the final position of the pivot element.
   */
-  public int partition (int [] data, int start, int end){
+  public static int partition (int [] data, int start, int end){
+  	for (int i = start; i < end+1; i++) {
+  		System.out.println(data[i]);
+  	}
+  	int pivot = end/2;
+  	System.out.println("index "+pivot+": "+data[pivot]);
+  	for (int i = start; i < end; i++) {
+  		if (data[i] > data[pivot]) {
+  			System.out.println(data[i]+" is GREATER than "+data[pivot]);
+  		}
+  		else {
+  			System.out.println(data[i]+" is LESS than or equal to "+data[pivot]);
 
+  		}
+  	}
+  	return -1;
+  }
+
+  public static void main(String[] args) {
+  	int[] test = new int[]{17,61,67,47,93,12,20,4,44,68};
+  	partition(test, 0, 9);
   }
 }
