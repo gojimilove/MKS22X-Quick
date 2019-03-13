eefc41fc2844f40ece812fea3cc66cbb29511e1e
Jessica Kim <jkim00@stuy.edu>
2019-03-13 05:13:12 -0400

    random number selected as pivot and more tests done, i think this version works

--- Quick.java
+++ Quick.java
@@ -3,40 +3,41 @@ public class Quick {
   *@return the index of the final position of the pivot element.
   */
   public static int partition (int [] data, int start, int end){
-  	// for (int i = start; i < end+1; i++) {
-  	// 	System.out.println(data[i]);
-  	// }
-  	int pivot = end/2;
-  	//System.out.println("index "+pivot+": "+data[pivot]);
+  	for (int i = start; i < end+1; i++) {
+  		System.out.println(data[i]);
+  	}
+  	int randPivot = (int)(Math.random()*(end+1));
+  	int pivot = randPivot;
+  	System.out.println("index "+pivot+": "+data[pivot]);
 
   	//pointers for current low and high
   	int low = start;
   	int high = end;
-  	while (low < end && high > -1) {
-  		//if the value is higher than pivot, swap with one at the high point and decrement high
- 			if (low < pivot && data[low] > data[pivot]) {
- 				int oldL = data[low];
- 				data[low] = data[high];
- 				data[high] = oldL;
- 				high--;
- 			}
- 			else {
- 				//increase low but if value is lower than pivot and index is higher, swap with pivot
- 				if (low > pivot && data[low] < data[pivot]) {
- 					int small = data[low];
- 					data[low] = data[pivot];
- 					data[pivot] = small;
- 					pivot = low;
- 				}
- 				low++;
- 			}
+  	while (low != high) {
+  		if (low == pivot) {
+  			int oldP = data[low];
+  			data[low] = data[high];
+  			data[high] = oldP;
+  			pivot = high;
+  		}
+  		if (data[low] > data[pivot]) {
+  			int oldL = data[low];
+  			data[low] = data[high];
+  			data[high] = oldL;
+  			if (pivot == high) pivot = low;
+  			high--;
+  		}
+  		else {
+  			low++;
+  		}
   	}
 
 
-  	// for (int i = start; i < end+1; i++) {
-  	// 	System.out.println(data[i]);
-  	// }
-  	// System.out.println("index "+pivot+": "+data[pivot]);
+
+  	for (int i = start; i < end+1; i++) {
+  		System.out.println(data[i]);
+  	}
+  	System.out.println("index "+pivot+": "+data[pivot]);
   	return pivot;
   }
 
