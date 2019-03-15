5da22553147889cca96f3a31b263e6ab1cda5c2a
Jessica Kim <jkim00@stuy.edu>
2019-03-15 11:22:46 -0400

    tried testing with mr. k's driver, it works ok but after 125000 with max value 10 i get a stack overflow error

--- Quick.java
+++ Quick.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Quick {
   /*
   *@return the index of the final position of the pivot element.
@@ -89,33 +91,69 @@ public class Quick {
   }
 
 
-  public static void main(String[] args) {
-  	int[] ary = new int[]{17,61,67,93,47,12,20,4,44,68};
-  	int[] ary3 = new int[]{27,29,6,13,45};
-  	int[] ary4 = new int[]{8,6,5,7,3,0,9};
-  	// System.out.println(partition(test1, 0, 9)+"\n\n");
-  	// System.out.println(partition(test2, 0, 4)+"\n\n");
-  	// System.out.println(partition(test3, 0, 6)+"\n\n");
-    int[]ary1 = new int[]{2,10,15,23,0,5};
-    //System.out.println(partition(ary, 0, 5)+"\n\n");
-    // System.out.println(quickselect(ary, 0));
-    // System.out.println(quickselect(ary, 1));
-    // System.out.println(quickselect(ary, 2));
-    // System.out.println(quickselect(ary, 3));
-    // System.out.println(quickselect(ary, 4));
-    // System.out.println(quickselect(ary, 5));
-    System.out.print("BEFORE SORTING: [");
-    for (int i = 0; i < ary.length; i++) {
-  		System.out.print(ary[i]);
-  		if (i < ary.length-1) System.out.print(", ");
-  	}
-  	System.out.print("]");
-    quicksort(ary);
-    System.out.print("\n\nAFTER SORTING: [");
-    for (int i = 0; i < ary.length; i++) {
-  		System.out.print(ary[i]);
-  		if (i < ary.length-1) System.out.print(", ");
-  	}
-  	System.out.print("]\n");
+  // public static void main(String[] args) {
+  // 	int[] ary = new int[]{17,61,67,93,47,12,20,4,44,68};
+  // 	int[] ary3 = new int[]{27,29,6,13,45};
+  // 	int[] ary4 = new int[]{8,6,5,7,3,0,9};
+  // 	// System.out.println(partition(test1, 0, 9)+"\n\n");
+  // 	// System.out.println(partition(test2, 0, 4)+"\n\n");
+  // 	// System.out.println(partition(test3, 0, 6)+"\n\n");
+  //   int[]ary1 = new int[]{2,10,15,23,0,5};
+  //   //System.out.println(partition(ary, 0, 5)+"\n\n");
+  //   // System.out.println(quickselect(ary, 0));
+  //   // System.out.println(quickselect(ary, 1));
+  //   // System.out.println(quickselect(ary, 2));
+  //   // System.out.println(quickselect(ary, 3));
+  //   // System.out.println(quickselect(ary, 4));
+  //   // System.out.println(quickselect(ary, 5));
+  //   System.out.print("BEFORE SORTING: [");
+  //   for (int i = 0; i < ary.length; i++) {
+  // 		System.out.print(ary[i]);
+  // 		if (i < ary.length-1) System.out.print(", ");
+  // 	}
+  // 	System.out.print("]");
+  //   quicksort(ary);
+  //   System.out.print("\n\nAFTER SORTING: [");
+  //   for (int i = 0; i < ary.length; i++) {
+  // 		System.out.print(ary[i]);
+  // 		if (i < ary.length-1) System.out.print(", ");
+  // 	}
+  // 	System.out.print("]\n");
+  // }
+
+  public static void main(String[]args){
+    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          Quick.quicksort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
+        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      }
+      System.out.println();
+    }
   }
+
 }
